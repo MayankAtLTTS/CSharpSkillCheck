931df7c018d654e5528ce52f1aa41990abf69d69
MayankAtLTTS <mayank.upadhyay@ltts.com>
2021-08-16 14:44:24 +0530

    First question solved

--- ProblemOne_IncorrectSolution/Program.cs
+++ ProblemOne_IncorrectSolution/Program.cs
@@ -13,12 +13,18 @@ namespace ProblemOne_IncorrectSolution
             //1. The following code find the sum of all the numbers between 0 and 10000
             //2. The sum of the numbers should be for only numbers that are divisble by 3 or 5
             int result = 0;
-            for (int i = 1; i <= 1000; i++)
+            try
             {
-                if (((i % 3) == 0) && ((i % 5) == 0))
+                for (int i = 1; i <= 1000; i++)
                 {
-                    result =+i;
+                    if (((i % 3) == 0) || ((i % 5) == 0))
+                    {
+                        result = +i;
+                    }
                 }
+            }
+            catch (Exception e) 
+                { Console.WriteLine("Exception: "+e); 
             }
             Console.WriteLine("The sum of all number divisble by 3 and 5 between 1 and 10000 is "+ result);
         }
